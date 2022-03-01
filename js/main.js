4006509f5f9fd08b1375bb3edd81263bb6ed53e2
gitadmen <94184870+gitadmen@users.noreply.github.com>
2022-03-01 12:40:32 +0100

    asd

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-$(document).ready(function () {
+$(document).ready(function() {
+
+
+
+    $('.side-menu').mouseenter(function() { // wysuwanie
+
+        $('.side-menu').animate({ marginLeft: '0px' }, 500)
+        $('.side-menu').addClass('open')
+    })
+
+    $('.side-menu').mouseleave(function() {
+        setTimeout(() => {
+            $('.side-menu').animate({ marginLeft: '-160px' }, 500)
+            $('.side-menu').removeClass('open')
+        }, 1000);
+    })
+
+
+    $('.slide').click(function() {
+
+        if ($(this).hasClass('open')) { // ("this" odnosi się do obiektu przekazanego przez selektor do wyrażenia nadrzędnego.) Jeżeli posiada klase open to:
+            $(this).removeClass('open'); // usuń klasę 'open' w obiekcie this (obiekt na którym operuje to wyrażenie)
+            $(this).find('.hidden').slideUp(); // w obiekcie this znajdź '.accordion-content' i wykonaj następujące instrukcje
+
+        } else {
+            $(this).addClass('open'); // utwórz i dodaj klasę 'open' w obiekcie this (obiekt na którym operuje to wyrażenie)
+            $(this).find('.hidden').slideDown(); // w obiekcie this znajdź '.accordion-content' i wykonaj następujące instrukcje
+        }
+
+    });
 
 
 
